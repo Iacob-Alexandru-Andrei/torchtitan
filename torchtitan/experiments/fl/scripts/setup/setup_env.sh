31f6a31ef684eacb7f9865e0643c5f1705972874
Andrej Jovanovic <andrej@jovanovic.co.za>
2026-01-23 13:32:33 +0000

    Change env versions

--- torchtitan/experiments/fl/scripts/setup/setup_env.sh
+++ torchtitan/experiments/fl/scripts/setup/setup_env.sh
@@ -6,7 +6,7 @@ uv pip install --pre --force-reinstall \
   --index-url https://download.pytorch.org/whl/nightly/cu128 \
   --extra-index-url https://pypi.nvidia.com \
   --only-binary=:all: \
-  "torch==2.10.0.dev20251110+cu128" \
-  "torchvision==0.25.0.dev20251110+cu128" \
+  "torch==2.10.0.dev20251120+cu128" \
+  "torchvision==0.25.0.dev20251120+cu128" \
   "nvidia-nvshmem-cu12==3.4.5"
 uv pip install "torchft-nightly==2025.10.11"
